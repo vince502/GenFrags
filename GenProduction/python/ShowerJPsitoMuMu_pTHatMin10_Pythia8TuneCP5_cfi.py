eac79ea98c9b3f423c0baa5a3fe482de1e7fa7e2
soohwan lee <62940130+vince502@users.noreply.github.com>
2025-01-15 14:19:02 +0900

    Create ShowerJPsitoMuMu_pTHatMin10_Pythia8TuneCP5_cfi.py
    updated Jpsi config

---
+++ GenProduction/python/ShowerJPsitoMuMu_pTHatMin10_Pythia8TuneCP5_cfi.py
@@ -0,0 +1,49 @@
+import FWCore.ParameterSet.Config as cms
+from Configuration.Generator.Pythia8CommonSettings_cfi import *
+from Configuration.Generator.MCTunesRun3ECM13p6TeV.PythiaCP5Settings_cfi import *
+
+# see https://www.pythia.org/latest-manual/OniaShowers.html
+
+generator = cms.EDFilter("Pythia8GeneratorFilter",
+                         pythiaPylistVerbosity = cms.untracked.int32(0),
+                         pythiaHepMCVerbosity = cms.untracked.bool(False),
+                         comEnergy = cms.double(5362.0),
+                         maxEventsToPrint = cms.untracked.int32(0),
+                         PythiaParameters = cms.PSet(
+        pythia8CommonSettingsBlock,
+        pythia8CP5SettingsBlock,
+        processParameters = cms.vstring(
+            'HardQCD:all = on',
+            'CharmoniumShower:all = on', # turn on for parton shower mode
+            'OniaShower:ldmeFac = 100.',
+            '443:onMode = off',
+            '443:onIfAny = 13 -13', # dimuon decay
+            'PhaseSpace:pTHatMin = 10.'
+            ),
+        parameterSets = cms.vstring('pythia8CommonSettings',
+                                    'pythia8CP5Settings',
+                                    'processParameters',
+                                    )
+        )
+)
+
+oniafilter = cms.EDFilter("PythiaFilter",
+    Status = cms.untracked.int32(2),
+    MaxEta = cms.untracked.double(4),
+    MinEta = cms.untracked.double(-4),
+    MinPt = cms.untracked.double(0.0),
+    ParticleID = cms.untracked.int32(443)
+)
+
+mumugenfilter = cms.EDFilter("MCParticlePairFilter",
+    Status = cms.untracked.vint32(1, 1),
+    MinPt = cms.untracked.vdouble(1., 1.),
+    MinP = cms.untracked.vdouble(2.5, 2.5),
+    MaxEta = cms.untracked.vdouble(2.5, 2.5),
+    MinEta = cms.untracked.vdouble(-2.5, -2.5),
+    ParticleCharge = cms.untracked.int32(-1),
+    ParticleID1 = cms.untracked.vint32(13),
+    ParticleID2 = cms.untracked.vint32(13)
+)
+
+ProductionFilterSequence = cms.Sequence(generator*oniafilter*mumugenfilter)
