4d5968ce17654574e0d7baa2ae55097f1488e6f0
soohwan <soohwan.lee@cern.ch>
2025-01-25 17:04:59 +0100

    updated eff specs

--- GenProduction/python/ShowerJPsitoMuMu_pTHatMin10_Pythia8TuneCP5_cfi.py
+++ GenProduction/python/ShowerJPsitoMuMu_pTHatMin10_Pythia8TuneCP5_cfi.py
@@ -1,3 +1,18 @@
+#Using cmsDriver command in CMSSW_14_1_6 cmsDriver.py Configuration/GenProduction/python/ShowerJPsitoMuMu_pTHatMin10_Pythia8TuneCP5_cfi.py --python_filename PsiToMuMuPiPi_GEN.py --eventcontent RAWSIM --datatier GEN-SIM --fileout file:Jpsi_GEN.root --conditions 141X_mcRun3_2024_realistic_ppRef5TeV_v7 --beamspot Realistic25ns13p6TeVEarly2023Collision --step GEN,SIM --geometry DB:Extended --era Run3_2024_ppRef --mc 
+
+#------------------------------------
+#GenXsecAnalyzer:
+#------------------------------------
+#Before Filter: total cross section = 1.811e+09 +- 1.021e+07 pb
+#Filter efficiency (taking into account weights)= (210) / (10000) = 2.100e-02 +- 1.434e-03
+#Filter efficiency (event-level)= (210) / (10000) = 2.100e-02 +- 1.434e-03    [TO BE USED IN MCM]
+#
+#After filter: final cross section = 3.802e+07 +- 2.605e+06 pb
+#After filter: final fraction of events with negative weights = 0.000e+00 +- 0.000e+00
+#After filter: final equivalent lumi for 1M events (1/fb) = 2.630e-05 +- 1.802e-06
+
+# event size 406 kB/evt, 16 ms/evt in AMD EPIC Genoa CPU
+
 import FWCore.ParameterSet.Config as cms
 from Configuration.Generator.Pythia8CommonSettings_cfi import *
 from Configuration.Generator.MCTunesRun3ECM13p6TeV.PythiaCP5Settings_cfi import *
